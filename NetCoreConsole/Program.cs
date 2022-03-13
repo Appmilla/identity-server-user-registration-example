834094212127b9b94505e21e9db304e3c3302a85
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2022-03-13 22:12:09 +0000

    Added projects

---
+++ NetCoreConsole/Program.cs
@@ -0,0 +1,123 @@
+﻿using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using Newtonsoft.Json.Linq;
+
+namespace NetCoreConsole
+{
+    public class Program
+    {        
+        static string _authority = "https://localhost:5001";
+        static string _api = "https://localhost:6001";
+
+        static OidcClient _oidcClient;
+        static HttpClient _apiClient = new HttpClient { BaseAddress = new Uri(_api) };
+
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        public static async Task MainAsync()
+        {
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|  Sign in with OIDC    |");
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to sign in...");
+            Console.ReadKey();
+
+            await Login();
+        }
+
+        private static async Task Login()
+        {
+            var browser = new SystemBrowser(45656);
+            string redirectUri = "http://127.0.0.1:45656";
+
+            var options = new OidcClientOptions
+            {
+                Authority = _authority,
+                ClientId = "native.code",
+                RedirectUri = redirectUri,
+                Scope = "openid profile email api1 offline_access",               
+                Browser = browser
+            };
+
+            _oidcClient = new OidcClient(options);
+            var result = await _oidcClient.LoginAsync(new LoginRequest());
+            ShowResult(result);
+            await NextSteps(result);
+        }
+
+
+        private static void ShowResult(LoginResult result)
+        {
+            if (result.IsError)
+            {
+                Console.WriteLine("\n\nError:\n{0}", result.Error);
+                return;
+            }
+
+            Console.WriteLine("\n\nClaims:");
+            foreach (var claim in result.User.Claims)
+            {
+                Console.WriteLine("{0}: {1}", claim.Type, claim.Value);
+            }
+
+            Console.WriteLine($"\nidentity token: {result.IdentityToken}");
+            Console.WriteLine($"access token:   {result.AccessToken}");
+            Console.WriteLine($"refresh token:  {result?.RefreshToken ?? "none"}");
+        }
+
+        private static async Task NextSteps(LoginResult result)
+        {
+            var currentAccessToken = result.AccessToken;
+            var currentRefreshToken = result.RefreshToken;
+
+            var menu = "  x...exit  c...call api   ";
+            if (currentRefreshToken != null) menu += "r...refresh token   ";
+
+            while (true)
+            {
+                Console.WriteLine("\n\n");
+
+                Console.Write(menu);
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.X) return;
+                if (key.Key == ConsoleKey.C) await CallApi(currentAccessToken);
+                if (key.Key == ConsoleKey.R)
+                {
+                    var refreshResult = await _oidcClient.RefreshTokenAsync(currentRefreshToken);
+                    if (refreshResult.IsError)
+                    {
+                        Console.WriteLine($"Error: {refreshResult.Error}");
+                    }
+                    else
+                    {
+                        currentRefreshToken = refreshResult.RefreshToken;
+                        currentAccessToken = refreshResult.AccessToken;
+
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine($"access token:   {refreshResult.AccessToken}");
+                        Console.WriteLine($"refresh token:  {refreshResult?.RefreshToken ?? "none"}");
+                    }
+                }
+            }
+        }
+
+        private static async Task CallApi(string currentAccessToken)
+        {
+            _apiClient.SetBearerToken(currentAccessToken);
+            var response = await _apiClient.GetAsync("WeatherForecast");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = JArray.Parse(await response.Content.ReadAsStringAsync());
+                Console.WriteLine("\n\n");
+                Console.WriteLine(json);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.ReasonPhrase}");
+            }
+        }
+    }
+}
